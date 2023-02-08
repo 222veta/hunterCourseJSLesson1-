d68c0abad8c2633c8fe986c317ca8bb78617f04c
Sussie <svetagoloshuk@gmail.com>
2023-02-08 18:04:03 +0300

    Operator lesson 10

---
+++ hunterJS1-.js
@@ -0,0 +1,39 @@
+"use strict";
+
+
+// 7 простое общение с пользователем
+// alert('Hello');  // hello   OK Вывод вверху браузера
+
+// const result = confirm("are you here?"); //вопрос? ок/отмена
+// console.log(result); // true/false
+
+// const answer = prompt(" вам есть 18?", "дефолтный ответ в строке для пользователя"); // вопрос?  строка для ввода ответа
+// console.log(answer); // ответ пользователя в строковом типе данных
+// console.log(typeof(answer));// тип данных переменной
+
+// const ans = +prompt("heh", ''); //плюс превращает в числовой тип данных
+
+
+
+//8 интерполяция
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);// можно использовать +, но будет слишком грамостко, если каждый рах прописывать "переменная" + "/" + "переменная" + "/" и тд
+
+// const user = 'Ivan';
+
+// alert(`привет ${user}`);
+
+
+//операторы
+
+let incr = 10;
+let dicr = 10;
+
+console.log(incr++); // 10, но потом изменится
+console.log(dicr--);// 10, но потом изменится
+console.log(++incr); // 11
+console.log(--dicr);// 9
+
+
+console.log('dc');
